9ab6e6a9796b284de7be009ffe81f614b09f3e08
Bruno Aguiar <brunoaguiar@Brunos-MacBook-Air.local>
2019-10-05 19:11:07 +0100

    findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -65,6 +65,14 @@ public class Seller implements Serializable{
 	public void setBaseSalary(Double baseSalary) {
 		this.baseSalary = baseSalary;
 	}
+	
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
 
 	@Override
 	public int hashCode() {
@@ -96,4 +104,5 @@ public class Seller implements Serializable{
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
+
 }
